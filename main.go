6ebe5b5da23f2db47e6aaef80bfe70a54c73364e
Puro Sur #CCD <allenam4173@student.laccd.edu>
2024-06-06 05:43:26 -0700

    main go ltn acc

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/PSClearn01/dfsGo/p2p"
+)
 
 func main() {
-	fmt.Println("A Huevo Puto!")
+	tr := p2p.NewTCPTransport(":3000")
+
+	if err := tr.ListenAndAccept(); err != nil {
+		log.Fatal(err)
+	}
+
+	select {}
+
 }
